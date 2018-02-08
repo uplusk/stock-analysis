8ccf533de4fb41449966931f7b2934d18858c541
uplusk <32155243+uplusk@users.noreply.github.com>
2018-02-08 15:52:32 +0530

    Added another notebook for multiple stocks
    The new notebook is kept for continuous analysis of different stocks.

--- stock_analysis.py
+++ stock_analysis.py
@@ -26,64 +26,90 @@ from stocker import Stocker
 quandl.ApiConfig.api_key = "sY8rR9LTz4kT5z2cs94v"
 
 # L&T Finance Holdings data
-stock_ltfh = quandl.get('NSE/LTFH')
-stock_ltfh = stock_ltfh.reset_index(level=0)
+stock_ltfh_df = quandl.get('NSE/LTFH')
+stock_ltfh_df = stock_ltfh_df.reset_index(level=0)
 
 # Motherson Sumi Data
-stock_mothersumi = quandl.get('NSE/MOTHERSUMI')
-stock_mothersumi = stock_mothersumi.reset_index(level=0)
+stock_mothersumi_df = quandl.get('NSE/MOTHERSUMI')
+stock_mothersumi_df = stock_mothersumi_df.reset_index(level=0)
 
 # Anantraj Ltd. Data
-stock_anantraj = quandl.get('NSE/ANANTRAJ')
-stock_anantraj = stock_anantraj.reset_index(level=0)
+stock_anantraj_df = quandl.get('NSE/ANANTRAJ')
+stock_anantraj_df = stock_anantraj_df.reset_index(level=0)
 
-#Building models LTFH using prophet
-stock_ltfh['ds'] = stock_ltfh['Date']
-stock_ltfh['y'] =stock_ltfh['Close']
-stock_ltfh['y-original'] = stock_ltfh['y']
-stock_ltfh['y'] = np.log(stock_ltfh['y'])
+#Building models LTFH using prophet, hence renaming columns
+stock_ltfh_df = stock_ltfh_df.rename(columns={'Date':'ds', 'Close':'y'})
 
+stock_mothersumi_df = stock_mothersumi_df.rename(columns={'Date':'ds', 'Close':'y'})
 
-stock_ltfh_df = stock_ltfh[['ds','y','y-original']]
+stock_anantraj_df = stock_anantraj_df.rename(columns={'Date':'ds', 'Close':'y'})
 
-print(stock_ltfh_df.tail())
+# verifying just to make sure
+print(stock_anantraj_df.tail())
+
+#historical plot
+#stock_ltfh_df.set_index('ds').y.plot()
+
+stock_anantraj_df['y_orig'] = stock_anantraj_df['y']
+stock_anantraj_df['y'] = np.log(stock_anantraj_df['y'])
+
+#print(stock_ltfh_df.tail())
+
+# plotting log-transformed data
+stock_anantraj_df.set_index('ds').y.plot()
+
+#Running Prophet
 model = Prophet()
-model.fit(stock_ltfh_df)
+model.fit(stock_anantraj_df)
 
 future_data = model.make_future_dataframe(periods = 60)
 
 
 forecast_data = model.predict(future_data)
-
-print("Log Transformed Data")
+"""print("Prophet Data")
 print(forecast_data[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].tail())
-
-
-forecast_data_orig = forecast_data # make sure we save the original forecast data
-forecast_data_orig['yhat'] = np.exp(forecast_data_orig['yhat'])
-forecast_data_orig['yhat_lower'] = np.exp(forecast_data_orig['yhat_lower'])
-forecast_data_orig['yhat_upper'] = np.exp(forecast_data_orig['yhat_upper'])
-
+"""
 model.plot(forecast_data)
 
 model.plot_components(forecast_data)
 
-print("Original Data")
-print(forecast_data_orig[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].tail()) 
+#Visualizing Prophet models
+stock_anantraj_df.set_index('ds', inplace=True)
+forecast_data.set_index('ds', inplace=True)
 
+combined_df = stock_anantraj_df.join(forecast_data[['yhat', 'yhat_lower','yhat_upper']], how = 'outer')
+#print(combined_df.tail())
 
+combined_df['yhat_rescaled'] = np.exp(combined_df['yhat'])
+print("Combined df")
+print(combined_df.head())
 
+#plotting combined df data
+combined_df[['y_orig','yhat_rescaled']].plot()
 
+#to plot a single prediction forecast, we are merging the graphs for a better result
+stock_anantraj_df.index = pd.to_datetime(stock_anantraj_df.index) #make sure our index as a datetime object
+connect_date = stock_anantraj_df.index[-2] #select the 2nd to last date
 
-"""
-#Building models Motherson Sumi using prophet
-stock_mothersumi= stock_mothersumi['Date']
+mask = (forecast_data.index > connect_date)
+prediction_df = forecast_data.loc[mask]
 
-stock_mothersumi['y'] =stock_mothersumi['Adj. Close']
+combined_df_final = stock_anantraj_df.join(prediction_df[['yhat', 'yhat_lower','yhat_upper']], how = 'outer')
+combined_df_final['yhat_rescaled'] = np.exp(combined_df_final['yhat'])
+combined_df_final['yhat_upper_rescaled'] = np.exp(combined_df_final['yhat_upper'])
+combined_df_final['yhat_lower_rescaled'] = np.exp(combined_df_final['yhat_lower'])
 
-#Building models Anantraj using prophet
-stock_anantraj['ds'] = stock_anantraj['Date']
+#time to plot
+fig, ax1 = plt.subplots()
+ax1.plot(combined_df_final.y_orig)
+ax1.plot(combined_df_final.yhat_rescaled, color='black', linestyle=':')
+ax1.fill_between(combined_df_final.index, np.exp(combined_df_final['yhat_upper']), np.exp(combined_df_final['yhat_lower']), alpha=0.5, color='darkgray')
+ax1.set_title('Stock Price(Orange) vs Stock Price Forecast (Black)')
+ax1.set_ylabel('Stock Prices (INR)')
+ax1.set_xlabel('Date')
 
-stock_anantraj['y'] =stock_anantraj['Adj. Close']
+L=ax1.legend() #get the legend
+L.get_texts()[0].set_text('Actual Price') #change the legend text for 1st plot
+L.get_texts()[1].set_text('Forecasted Price') #change the legend text for 2nd plot
 
-"""
+print(combined_df.tail(60))
